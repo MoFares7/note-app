2242636f12f18e43cfe5d1b47392da343e2a8d2d
MoFares7 <faresdabbas1@gmail.com>
2023-03-16 17:58:38 +0200

    initialization to Sidebar

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./css/Sidebar.css"
+
+export default function Sidebar() {
+        return (
+                <section className="pane sidebar">
+                        <div className="sidebar--header">
+                                <h3>Note</h3>
+                                <button className="new--note">+</button>
+
+                        </div>
+                
+                </section>
+        )
+}
