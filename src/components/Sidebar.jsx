781116354d2980e5201bc95a4c7217cd5ec9a1af
MoFares7 <faresdabbas1@gmail.com>
2023-03-16 18:20:08 +0200

    initialization to Editor

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import "./css/Sidebar.css"
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+        const noteElements = props.notes.map((note, index) => (
+                <div key={note.id}>
+                        <div
+
+                                className={`title ${note.id === props.currentNote.id ? "selected-note" : ""
+                                        }`}
+                                onClick={() => props.setCurrentNoteId(note.id)}
+                        >
+                                <h4 className="text-snippet">Note {index + 1}</h4>
+                        </div>
+                </div>
+        ))
         return (
                 <section className="pane sidebar">
                         <div className="sidebar--header">
                                 <h3>Note</h3>
                                 <button className="new--note">+</button>
-
+                                {noteElements}
                         </div>
-                
+
                 </section>
         )
 }
